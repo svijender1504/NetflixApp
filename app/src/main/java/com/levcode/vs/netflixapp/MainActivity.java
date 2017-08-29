6593ad8df9b3916a4bd3f8e3d82d3f205b34b1dd
Vijender Singh <vijendersingh@Vijenders-MacBook-Pro.local>
2017-08-29 19:56:32 -0400

    First commit

--- app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
+++ app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.levcode.vs.netflixapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtSearch;
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtSearch=(EditText)findViewById(R.id.txtSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
