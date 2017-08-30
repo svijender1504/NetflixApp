7a76698e687e1a5ebe430ba2f99f788743dbf474
Vijender Singh <vijendersingh@Vijenders-MacBook-Pro.local>
2017-08-29 20:57:51 -0400

    commit 6

--- app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
+++ app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -21,13 +22,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtSearch;
     Button btnSearch;
+    private static final String TAG_TITLE = "show_title";
+    private static final String TAG_RELEASE = "release_year";
+    private static final String TAG_RATING = "rating";
+    private static final String TAG_POSTER = "poster";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -40,51 +44,88 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-}
 
-class Search extends AsyncTask <String, String, String> {
 
-    String text;
-    StringBuilder strBuilder1 = new StringBuilder();
-    JSONArray jsonArray;
-    JSONObject jsonObj;
+    class Search extends AsyncTask<String, String, String> {
 
-    int n;
-    String show_title = null, release_year = null, rating = null, poster = null;
-    String ushow_title = null, urelease_year = null, urating = null, uposter = null;
-    ArrayList<String> arrLst0 = new ArrayList<String>();
-    ArrayList<String> arrLst1 = new ArrayList<String>();
-    ArrayList<String> arrLst2 = new ArrayList<String>();
-    ArrayList<String> arrLst3 = new ArrayList<String>();
-    ArrayList<String> arrLst4 = new ArrayList<String>();
-    ArrayList<String> arrLst5 = new ArrayList<String>();
-    ArrayList<String> arrLst6 = new ArrayList<String>();
-    ArrayList<String> arrLst7 = new ArrayList<String>();
-    @Override
-    protected String doInBackground(String... strings) {
-        URL url = null;
-        try {
-            url = new URL("http://netflixroulette.net/api/api.php?actor=Nicolas%20Cage");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String read1;
-            while ((read1 = bufferedReader.readLine()) != null) {
-                strBuilder1.append(read1);
+        EditText txtSearch;
+        String text;
+        StringBuilder strBuilder1 = new StringBuilder();
+        JSONArray jsonArray;
+        JSONObject jsonObj;
+
+        int n;
+
+        String show_title = null, release_year = null, rating = null, poster = null;
+        String ushow_title = null, urelease_year = null, urating = null, uposter = null;
+        ArrayList<String> arrLst0 = new ArrayList<String>();
+        ArrayList<String> arrLst1 = new ArrayList<String>();
+        ArrayList<String> arrLst2 = new ArrayList<String>();
+        ArrayList<String> arrLst3 = new ArrayList<String>();
+        ArrayList<String> arrLst4 = new ArrayList<String>();
+        ArrayList<String> arrLst5 = new ArrayList<String>();
+        ArrayList<String> arrLst6 = new ArrayList<String>();
+        ArrayList<String> arrLst7 = new ArrayList<String>();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            try {
+
+                url = new URL("http://netflixroulette.net/api/api.php?actor=Nicolas%20Cage");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String read1;
+                while ((read1 = bufferedReader.readLine()) != null) {
+                    strBuilder1.append(read1);
+                }
+                return null;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return null;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return strBuilder1.toString();
+
         }
-        return strBuilder1.toString();
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            txtSearch = (EditText) findViewById(R.id.txtSearch);
+
+
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(result);
+                jsonObj = jsonObject.getJSONObject("request_result");
+                jsonArray = jsonObj.getJSONArray("results");
+                n = jsonArray.length();
+
+                for (int i = 0; i < n; i++) {
+                    //nume= jsonObj.getString(TAG_NAME);
+                    show_title = jsonArray.getJSONObject(i).getString(TAG_TITLE);
+                    release_year = jsonArray.getJSONObject(i).getString(TAG_RELEASE);
+                    rating = jsonArray.getJSONObject(i).getString(TAG_RATING);
+                    poster = jsonArray.getJSONObject(i).getString(TAG_POSTER);
+
+                    arrLst0.add(show_title);
+                    arrLst1.add(release_year);
+                    arrLst2.add(rating);
+                    arrLst3.add(poster);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
-
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-    }
-
 }
