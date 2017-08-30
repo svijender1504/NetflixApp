1438686285070b1b6321d87ab5ec0789c0ac8068
Vijender Singh <vijendersingh@Vijenders-MacBook-Pro.local>
2017-08-29 20:12:28 -0400

    commit 3

--- app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
+++ app/src/main/java/com/levcode/vs/netflixapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.levcode.vs.netflixapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +26,35 @@ public class MainActivity extends AppCompatActivity {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                new Search().execute("");
+
             }
         });
 
+    }
+}
+
+class Search extends AsyncTask <String, String, String> {
+
+    String text;
+    StringBuilder strBuilder1 = new StringBuilder();
+    JSONArray jsonArray;
+    JSONObject jsonObj;
+
+    int n;
+    String show_title = null, release_year = null, rating = null, poster = null;
+    String ushow_title = null, urelease_year = null, urating = null, uposter = null;
+    ArrayList<String> arrLst0 = new ArrayList<String>();
+    ArrayList<String> arrLst1 = new ArrayList<String>();
+    ArrayList<String> arrLst2 = new ArrayList<String>();
+    ArrayList<String> arrLst3 = new ArrayList<String>();
+    ArrayList<String> arrLst4 = new ArrayList<String>();
+    ArrayList<String> arrLst5 = new ArrayList<String>();
+    ArrayList<String> arrLst6 = new ArrayList<String>();
+    ArrayList<String> arrLst7 = new ArrayList<String>();
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
     }
 }
