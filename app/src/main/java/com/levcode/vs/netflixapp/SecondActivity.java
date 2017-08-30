f08ce4b819085242e5d955f445ab6189ae5959bf
Vijender Singh <vijendersingh@Vijenders-MacBook-Pro.local>
2017-08-29 21:50:49 -0400

    commit 9

---
+++ app/src/main/java/com/levcode/vs/netflixapp/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.levcode.vs.netflixapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by vijendersingh on 2017-08-29.
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
